f5677b868f0e85807a2c087cc6483097ac5c6d92
Serena Jeblee <sjeblee@cs.cmu.edu>
2013-04-04 12:27:33 -0400

    updated notes

--- project/levtoegy.sh
+++ project/levtoegy.sh
@@ -1,13 +1,13 @@
 #!bin/bash/
 # Convert Levantine to be more like Egyptian
 # sjeblee@cs.cmu.edu
-# Last Modified: 14 March 2013
+# Last Modified: 19 March 2013
 
 #TODO
 #to be able to: fii
 # shuu 'aam verb -> verb eyh
 # rH verb -> H verb
-# ma'ai -> ma'ayya
+# maa verb -> ma-verb-sh
 
 #word reordering:
 # imta verb -> verb imta
@@ -17,12 +17,14 @@
 
 file="levantine.txt"
 
+sed 's/ معي / معيا /g' $file | \ #ma'i to ma'ayya
 sed 's/ وين / فين /g' $file | \ #weyn to feyn
 sed 's/ ليش / ليه /g' $file | \ #leysh to leyh
-sed 's/ shlon/ izzay/g' $file | \ #shlon to izzay
-sed 's/ kayf / izzay /g' $file | \ #kayf to izzay
-sed 's/ kayfk / izzayk /g' $file | \ #kayfak to izzayak
-sed 's/ qdi:sh / bika:m /g' $file | \ #qadiish to bikam
-sed 's/ hllq / delwati: /g' $file | \ #hallaq to delwa'ti
-sed 's/ mni:H / kwayis /g' $file | \ #mniiH to kwayis
-sed 's/ mba:rH / imba:rH /g' $file > lev-converted.txt #mbarH to imbaraH
+sed 's/شلون/ إزي/g' $file | \ #shlon to izzay
+sed 's/ كيف / إزي /g' $file | \ #kayf to izzay
+sed 's/ كيفك / إزيك /g' $file | \ #kayfak to izzayak
+sed 's/ قديش / بكام /g' $file | \ #qadiish to bikam
+sed 's/ هلق / دلوقتي /g' $file | \ #hallaq to delwa'ti
+sed 's/ منيح / كويس /g' $file | \ #mniiH to kwayis
+#sed 's/رح [a-z] / ح /g' $file | \ #rH verb to H-verb
+sed 's/ مبارح / امبارح /g' $file > lev-converted.txt #mbarH to imbaraH
