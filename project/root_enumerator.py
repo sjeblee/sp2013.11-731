44091bb0d8bef119f1d5ec2549a293055ddecd56
Serena Jeblee <sjeblee@cs.cmu.edu>
2013-04-19 20:14:26 -0400

    updated foma stuff

--- project/root_enumerator.py
+++ project/root_enumerator.py
@@ -20,8 +20,15 @@ def main(args):
 	waw = u"و"
 	ya = u"ي"
 	tm = u"ة"
+	hamza = u"ء"
 	longv = set(list(u"او"))
-	out = []
+	out = set([])
+	if pos == "Noun":
+		#Open file and read lines into other_nouns
+		other_nouns = open("data/egy/egytrain.nouns").readlines()
+		#Remove newline characters and add to output word set
+		for item in other_nouns:
+			out.add(item.strip())
 	for x in alphabet:
 		for y in alphabet:
 			for z in alphabet:
@@ -36,23 +43,43 @@ def main(args):
 				#Rule 3: Check for two vowel case, with waw+alif-maqsura, override rule 1
 				if (x not in longv) and (y == waw) and (z == alif_maq):
 					vflag = 0
-				if (not (x==y)) and (not (x==y==z)) and (vflag < 2):
+				if (not (x==y)) and (not (x==y==z)) and (vflag < 2) and (not (y == hamza)):
 					#Add 3-letter root to out list
-					out.append((x+y+z).encode('utf-8'))
+					out.add((x+y+z).encode('utf-8'))
 					#Generate verb forms
 					if pos == "Verb":
-						out.append((x+alif+y+z).encode('utf-8')) # V3
-						out.append((alif+x+y+z).encode('utf-8')) # V4
-						out.append((ta+x+y+z).encode('utf-8')) # V5
-						out.append((ta+x+alif+y+z).encode('utf-8')) # V6
-						out.append((alif+nun+x+y+z).encode('utf-8')) # V7
-						out.append((alif+x+ta+y+z).encode('utf-8')) # V8
-						out.append((alif+sin+ta+x+y+z).encode('utf-8')) # V10
+						out.add((x+alif+y+z).encode('utf-8')) # V3
+						if (not (x == hamza)):
+							out.add((alif+x+y+z).encode('utf-8')) # V4
+							out.add((ta+x+y+z).encode('utf-8')) # V5
+							out.add((ta+x+alif+y+z).encode('utf-8')) # V6
+							out.add((alif+nun+x+y+z).encode('utf-8')) # V7
+							out.add((alif+x+ta+y+z).encode('utf-8')) # V8
+							out.add((alif+sin+ta+x+y+z).encode('utf-8')) # V10
 					elif pos == "Noun":
-						out.append((x+alif+y+z).encode('utf-8')) # nominalization 
-						out.append((mim+x+y+waw+z).encode('utf-8')) # nominalization
-						out.append((x+y+alif+z).encode('utf-8'))
-						out.append((x+y++alif+z+ya).encode('utf-8'))
+						if (not (x == alif)) and (not (y == alif)):
+							out.add((x+alif+y+z).encode('utf-8')) # nominalization 
+							out.add((mim+x+alif+y+z).encode('utf-8'))
+						out.add((x+y+waw+z).encode('utf-8'))
+						if (not (x == hamza)):
+							out.add((mim+x+y+waw+z).encode('utf-8')) # nominalization 
+							out.add((mim+x+y+z).encode('utf-8')) 
+							out.add((ta+x+y+ya+z).encode('utf-8'))
+							out.add((ta+x+y+z).encode('utf-8'))
+						if (not (y == alif)) and (not (z == alif)):
+							out.add((x+y+alif+z).encode('utf-8'))
+							out.add((x+y+alif+z+ya).encode('utf-8'))
+							if (not (x == hamza)):
+								out.add((ta+x+y+alif+z).encode('utf-8'))
+								out.add((alif+nun+x+y+alif+z).encode('utf-8'))
+								out.add((alif+sin+ta+x+y+alif+z).encode('utf-8'))
+								if (not (x == alif)) :
+									out.add((alif+x+y+alif+z).encode('utf-8'))
+					elif pos == "Adj"
+						out.add((x+y+z).encode('utf-8'))
+						out.add((x+y+ya+z).encode('utf-8'))
+						out.add((x+alif+y+ya).encode('utf-8'))
+						
 	#Print roots to stdout
 	for item in out:
 		print item+' '+pos+'Inf;'
